a3e1c83e01f1c1644808e702443176eece4fab04
Natanael Alves <natan.alves.silva16@gmail.com>
2026-01-24 11:53:24 -0400

    Adicionando funções básicas aritméticas

---
+++ codigo.py
@@ -0,0 +1,19 @@
+def soma (a, b):
+    return a + b 
+
+def subtracao (a, b):
+    return a - b
+
+def multiplicacao (a, b):
+    return a * b
+
+def divisao (a, b):
+    return a / b
+
+valor_1 = 10
+valor_2 = 20
+
+print("valor 1 somado com o valor 2:", soma (valor_1, valor_2))
+print("valor 1 subtraido o valor 2:", subtracao(valor_1, valor_2))
+print("valor 1 multiplicado pelo o valor 2:", multiplicacao(valor_1, valor_2))
+print("valor 1 dividido pelo o valor 2:", divisao(valor_1, valor_2))
