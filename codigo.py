7fcd82ba0b51c3e85daab3e6a0ab6640457982ed
Natanael Alves <natan.alves.silva16@gmail.com>
2026-01-27 05:14:33 -0400

    Implementando o funcionamento das operações

--- codigo.py
+++ codigo.py
@@ -18,15 +18,29 @@ def exibir_menu():
     print("4 - Divisão")
     print("0 - Sair")
 
-valor_inicial = float(input("Digite o valor inicial: "))
+resultado_atual = float(input("Digite o valor inicial: "))
 
 while  True:
-    print(f"Resultado atual: {valor_inicial}")
+    print(f"Resultado atual: {resultado_atual}")
 
     exibir_menu()
 
     opcao_escolhida = input("Escolha uma opção: ")
 
-    if opcao_escolhida == "0":
-        print("Encerrando a calculadora. Até mais! ")
+    if opcao_escolhida == "0": 
         break
+
+    valor_operando = float(input("Digite o valor do operando: "))
+
+    if opcao_escolhida == "1":
+        resultado_atual = soma(resultado_atual, valor_operando)
+    elif opcao_escolhida == "2":
+        resultado_atual = subtracao(resultado_atual, valor_operando)
+    elif opcao_escolhida == "3":
+        resultado_atual = multiplicacao(resultado_atual, valor_operando)
+    elif opcao_escolhida == "4":
+        resultado_atual = divisao(resultado_atual, valor_operando)
+
+    
+
+    print("Encerrando a calculadora. Até mais! ")
